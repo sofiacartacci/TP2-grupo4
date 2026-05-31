ea3c10cb4b36458d0549098c77fa8d250c078db0
scartacci <sofiabelencartacci@gmail.com>
2026-05-31 14:36:12 -0300

    agregar config con variables de entorno y desinstale dotenv

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const { env } = process;
+
+const SERVER_PORT = env.SERVER_PORT || 8000;
+
+export { SERVER_PORT };
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import router from './routes/router.js';
-import { notFound } from './middlewares/notFound.js';
-import { sequelize } from './models/index.js';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import router from "./routes/router.js";
+import { notFound } from "./middlewares/notFound.js";
+import { sequelize } from "./models/index.js";
+import { SERVER_PORT } from "./config/config.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/api', router); 
-app.use(notFound);  
-
-const PORT = process.env.PORT || 8000;
+app.use(router);
+app.use(notFound);
 
 try {
   await sequelize.authenticate();
-  console.log('Conexión a la base establecida.');
-  await sequelize.sync({ force: false });
-  console.log('Tablas sincronizadas.');
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log("Conexion a la base establecida.");
+  await sequelize.sync({ alter: true });
+  console.log("Tablas sincronizadas.");
+  app.listen(SERVER_PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${SERVER_PORT}`);
   });
 } catch (error) {
-  console.error('Error al iniciar el servidor:', error);
+  console.error("Error al iniciar el servidor:", error);
 }
