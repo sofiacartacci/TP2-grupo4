2d36b7746467e1894c07011dbe5634df426ee137
Martin <mendez.monsalvo.m@gmail.com>
2026-06-16 20:03:38 -0300

    verificar existencia de cine y pelicula al crear funciones

--- services/funcionService.js
+++ services/funcionService.js
@@ -5,6 +5,22 @@ class FuncionService {
     this.pelicula = peliculaModel;
   }
 
+  verificarRelaciones = async (cineId, peliculaId) => {
+    const cine = await this.cine.findOne({ where: { id: cineId } });
+    if (!cine) {
+      const error = new Error("El cine indicado no existe");
+      error.status = 404;
+      throw error;
+    }
+
+    const pelicula = await this.pelicula.findOne({ where: { id: peliculaId } });
+    if (!pelicula) {
+      const error = new Error("La película indicada no existe");
+      error.status = 404;
+      throw error;
+    }
+  };
+
   getAllFunciones = async () => {
     const funciones = await this.funcion.findAll({
       include: [
@@ -27,11 +43,13 @@ class FuncionService {
   };
 
   createFuncion = async ({ fecha, hora, sala, precio, cineId, peliculaId }) => {
+    await this.verificarRelaciones(cineId, peliculaId);
     const funcion = await this.funcion.create({ fecha, hora, sala, precio, cineId, peliculaId });
     return funcion;
   };
 
   updateFuncion = async (id, { fecha, hora, sala, precio, cineId, peliculaId }) => {
+    await this.verificarRelaciones(cineId, peliculaId);
     await this.funcion.update({ fecha, hora, sala, precio, cineId, peliculaId }, { where: { id } });
     return "Funcion actualizada";
   };
